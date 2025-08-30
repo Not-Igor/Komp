e1deb84c9c93396095f0b5425f8ab4fdd011bf58
Egor-Kolomiets <95211298+Not-Igor@users.noreply.github.com>
2025-08-30 02:07:29 +0200

    add FriendRequestRepository implementation

---
+++ src/main/java/com/egor/back_end/repository/FriendRequestRepository.java
@@ -0,0 +1,17 @@
+package com.egor.back_end.repository;
+
+import com.egor.back_end.model.FriendRequest;
+import com.egor.back_end.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface FriendRequestRepository extends JpaRepository<FriendRequest, Long> {
+    // Find all requests sent to a specific user with a specific status
+    List<FriendRequest> findByReceiverAndStatus(User receiver, FriendRequest.Status status);
+    // Find a specific request between sender and receiver
+    Optional<FriendRequest> findBySenderAndReceiver(User sender, User receiver);
+    // List of requests that a user has sent
+    List<FriendRequest> findBySender(User sender);
+}
