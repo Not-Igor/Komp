e4da1fe4b9a7c9515768b0e58cace7ef00826add
Egor-Kolomiets <95211298+Not-Igor@users.noreply.github.com>
2025-08-30 02:34:54 +0200

    add FriendDto class definition

---
+++ src/main/java/com/egor/back_end/dto/user/FriendDto.java
@@ -0,0 +1,39 @@
+package com.egor.back_end.dto.user;
+
+public class FriendDto {
+    private Long id;
+    private String username;
+    private String email;
+
+    public FriendDto() {}
+
+    public FriendDto(Long id, String username, String email) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
