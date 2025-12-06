70461abd821ddb655da3d50ccdf638f961b8d088
Egor-Kolomiets <95211298+Not-Igor@users.noreply.github.com>
2025-12-07 00:27:30 +0100

    refactor: remove lombok from UserUpdateDto to debug 500 error

--- src/main/java/com/egor/back_end/dto/user/UserUpdateDto.java
+++ src/main/java/com/egor/back_end/dto/user/UserUpdateDto.java
@@ -1,16 +1,40 @@
 package com.egor.back_end.dto.user;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
 public class UserUpdateDto {
     private String newUsername;
     private String currentPassword;
     private String newPassword;
+
+    public UserUpdateDto() {
+    }
+
+    public UserUpdateDto(String newUsername, String currentPassword, String newPassword) {
+        this.newUsername = newUsername;
+        this.currentPassword = currentPassword;
+        this.newPassword = newPassword;
+    }
+
+    public String getNewUsername() {
+        return newUsername;
+    }
+
+    public void setNewUsername(String newUsername) {
+        this.newUsername = newUsername;
+    }
+
+    public String getCurrentPassword() {
+        return currentPassword;
+    }
+
+    public void setCurrentPassword(String currentPassword) {
+        this.currentPassword = currentPassword;
+    }
+
+    public String getNewPassword() {
+        return newPassword;
+    }
+
+    public void setNewPassword(String newPassword) {
+        this.newPassword = newPassword;
+    }
 }
